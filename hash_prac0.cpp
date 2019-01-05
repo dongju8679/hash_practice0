70721361459df1d64c911f23e14ce5c09c962c24
dongju3 <dongju8679@gmail.com>
2019-01-05 19:23:15 +0900

    add hash function

--- hash_prac0.cpp
+++ hash_prac0.cpp
@@ -4,6 +4,18 @@
 
 using namespace std;
 
+unsigned long myhash(const char *str)
+{
+	unsigned long hash = 5381;
+	int c;
+
+	while (c = *str++)
+	{
+		hash = (((hash << 5) + hash) + c) % MAX_TABLE;
+	}
+	return hash % MAX_TABLE;
+}
+
 int main(void)
 {
     int ret = 0;
